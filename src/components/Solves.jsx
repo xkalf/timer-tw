2d8b4e85042be7e44abd3dc8607b0af8afe5e689
xkalf <amroos988@gmail.com>
2022-03-07 20:16:34 +0800

    1

--- src/components/Solves.jsx
+++ src/components/Solves.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Solves = () => {
   return (
-    <div className="flex items-center justify-between p-3">
+    <div className="flex items-center justify-between px-0 xl:px-3">
       <span className="flex items-center whitespace-nowrap text-base text-textColor lg:text-lg">
         1. 1.22
       </span>
-      <div className="flex w-5/12 gap-0.5 lg:gap-1 ">
-        <button className="cursor-pointer text-base text-plusButton lg:text-lg">
+      <div className="flex w-5/12 gap-0.5 xl:gap-1 ">
+        <button className="cursor-pointer text-base text-plusButton xl:text-lg">
           +2
         </button>
-        <button className="cursor-pointer text-base text-textColor lg:text-lg">
+        <button className="cursor-pointer text-base text-textColor xl:text-lg">
           DNF
         </button>
-        <button className="cursor-pointer text-base text-[#ca3a3a] lg:text-lg">
+        <button className="cursor-pointer text-base text-[#ca3a3a] xl:text-lg">
           X
         </button>
       </div>
--- src/components/SolvesList.jsx
+++ src/components/SolvesList.jsx
@@ -3,7 +3,7 @@ import Solves from "./Solves";
 
 const SolvesList = () => {
   return (
-    <div className="solves-list flex h-[30%] flex-col-reverse overflow-x-hidden overflow-y-scroll rounded-[20px] bg-solvesList p-4 text-lg text-textColor">
+    <div className="solves-list flex h-[30%] flex-col-reverse overflow-x-hidden overflow-y-scroll rounded-[20px] bg-solvesList p-2 text-lg text-textColor xl:p-4">
       <Solves />
       <Solves />
       <Solves />
